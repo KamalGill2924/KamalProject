4e146290f70c91340b664035285d3407ccdea966
Kamaljeet Kaur <850001641@nzse.ac.nz>
2024-11-04 19:35:09 +1300

    Add project files.

---
+++ LanghamHotelManagementSystem/LanghamHotelManagementSystem/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LanghamHotelManagementSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Langham Hotel Management System");
+        }
+    }
+}
